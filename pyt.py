14a0364e8b8a6be863d0aa40497e68cb98872612
Md Azizul <khanazizul1199@gmail.com>
2024-08-25 20:03:21 +0600

    first commit

---
+++ pyt.py
@@ -0,0 +1,28 @@
+# this is a comment line
+''' 
+this is multi line comment 
+'''
+# data type in py
+#string #integer float list tuple boolean dictionary
+num = "number"
+string = "string"
+lists = [1,2,3,4]
+tuplee = (1,2,3)
+boolean = (True,False)
+dictionary = {
+    "a": "hello",
+    "b" : {
+        "nested":"nested"
+        }
+    }
+# if else statements
+# loop in py (for loop)
+for props in dictionary : 
+    print(props)
+print(dictionary["b"])   
+  # range loop
+for num in range(10,1000) :
+   print(num)
+  #  while loop 
+     #  function in py
+     # module 
