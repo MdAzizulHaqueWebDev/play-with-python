27f02f94eb7de0463017eb3c7e15c424c9938c76
Azizul <khanazizul1199@gmail.com>
2024-12-26 13:54:55 +0600

    format code

--- first-web-app-by-Python/app.py
+++ first-web-app-by-Python/app.py
@@ -3,14 +3,15 @@ from flask import render_template
 app  = Flask(__name__)
 
 @app.route('/')
-@app.route('/about')
-
-
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+
 def about():
     return render_template('about.html')
- 
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
