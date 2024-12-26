b5c62272d50826d3cb707f386b8553b2576e55fd
Azizul <khanazizul1199@gmail.com>
2024-12-26 13:39:29 +0600

    add about me

--- first-web-app-by-Python/app.py
+++ first-web-app-by-Python/app.py
@@ -3,8 +3,13 @@ from flask import Flask
 app  = Flask(__name__)
 
 @app.route('/')
+@app.route('/about')
+
+def about():
+    return "Hey there , I'm a autodidact programmer who is learning to code in Python and Flask and JS is my heart"
+
 def home():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
